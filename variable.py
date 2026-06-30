4ddb3a5fee64e3622285c1531daae300b8648d56
Karthi2028187 <n50738468@gmail.com>
2026-06-30 12:45:32 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,14 @@
+a=int(input("Enter the value of a:"))
+b=int(input("Enter the value of b:"))
+c=int(input("Enter the value of c:"))
+if(a>b & a>c):
+    print("a is greater")
+elif(b>a & b>c):
+    print("b is greater")
+else:
+    print("c is greater")
+
+
+
+
+   
